ab3e694659b76d60e0a27c26d3f92a89258e80da
Her1726194 <34738762+Her1726194@users.noreply.github.com>
2021-01-08 15:24:48 +0100

    Admin panel errors fixed

--- js/admin_panel.js
+++ js/admin_panel.js
@@ -44,43 +44,43 @@ function deleteUser(userInfo, userId) {
                     });
                 });
 	    }
-    })
-    
-    function blockUser(userInfo, userId) {
-        return new Promise((resolve, reject) => {
-            if (!userId) {
-                errorHanlder.createErrorResponse(userInfo, 400, "Bad Request, No User Id")
-                .then(err => {
-                    reject(err);
-                });
-            }
-
-            Promise.all ([
-                db_connector.geUserById()
-        
-            ]).then(results => {
-                if (!(results[0].Userrole === 'admin' || results[0].Userrol === 'vendor')) {
-        
-                    db_conector.blockUser(userId)
-                        .then(rows => {
-        
-                            admin_panel.createAdminPanel(userInfo)
-                        })
-                        .catch(err => {
-                            errorHandler.createErrorResponse(err, userInfo, 500, "Internal Server Error")
-                            .then(html => {
-                                console.log(err);
-                                reject(html);
-                            });
-                        });
-                }
-            })
-        })
-    }
-
-            
+    })      
     });
 }
+
+
+function blockUser(userInfo, userId) {
+    return new Promise((resolve, reject) => {
+        if (!userId) {
+            errorHanlder.createErrorResponse(userInfo, 400, "Bad Request, No User Id")
+            .then(err => {
+                reject(err);
+            });
+        }
+
+        Promise.all ([
+            db_connector.geUserById()
+    
+        ]).then(results => {
+            if (!(results[0].Userrole === 'admin' || results[0].Userrole === 'vendor')) {
+    
+                db_conector.blockUser(userId)
+                    .then(rows => {
+    
+                        admin_panel.createAdminPanel(userInfo)
+                    })
+                    .catch(err => {
+                        errorHandler.createErrorResponse(err, userInfo, 500, "Internal Server Error")
+                        .then(html => {
+                            console.log(err);
+                            reject(html);
+                        });
+                    });
+            }
+        })
+    })
+}
+
 
 module.exports = {
     createAdminPanel,
