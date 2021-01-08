c32d9feb6b23fc5ae9c3ee083398b12688707bb2
HexadimensionalerAlp <38053277+HexadimensionalerAlp@users.noreply.github.com>
2021-01-08 12:15:57 +0100

    fixed adminPanel

--- js/admin_panel.js
+++ js/admin_panel.js
@@ -1,14 +1,11 @@
 const tools = require('./tools');
 const db_connector = require('./database_connection');
-const errorHandler = require('./errorHandler');
-const htmlParser = require('node-html-parser');
-
 
 function createAdminPanel(userInfo) {
     return new Promise((resolve, reject) => {
         Promise.all([
-           tools.readHtmlAndAddNav(userinfo, 'admin_panel.html'),
-           db_connector.getAllUsers()ear
+           tools.readHtmlAndAddNav(userInfo, 'admin_panel.html'),
+           db_connector.getAllUsers()
         ]).then(results => {
                 const table = tools.buildUserTable(results[1]);
                 resolve(results[0].replace('{ adminPanel }', table))
@@ -18,8 +15,6 @@ function createAdminPanel(userInfo) {
             });
         });
 }
-
-
 
 module.exports = {
     createAdminPanel,
