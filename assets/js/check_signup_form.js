30b66a0800ec9c54b1735cc3494438767b9baa7b
HunterZ1994 <martin.thuerauf.1994@gmail.com>
2021-01-15 11:05:45 +0100

    no error html

--- assets/js/check_signup_form.js
+++ assets/js/check_signup_form.js
@@ -5,13 +5,13 @@ function checkPassword() {
     console.log(password2);
     if (password === password2 && password !== "" && password) {
         const element = $('#passwordCorrect').css("color", "green");
-        console.log(element);
+        // console.log(element);
         element.innerHTML = "Passwörter stimmen überein!";
         // element.style.color = 'green';
         document.getElementById("submit").removeAttribute("disabled")
     } else {
         const element = $("#passwordCorrect").css("color", "red");
-        console.log(element);
+        // console.log(element);
         element.innerHTML = "Passwörter stimmen nicht überein!";
         // element.style.color = 'red';
         document.getElementById("submit").setAttribute("disabled", true)
